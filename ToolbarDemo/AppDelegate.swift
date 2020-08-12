6b237ab0782d71ad6a4c353037f05bf04af5ad45
Jerry Krinock <jerry@sheepsystems.com>
2020-08-12 09:57:12 -0700

    Initial Commit

---
+++ ToolbarDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ToolbarDemo
+//
+//  Created by Jerry on 20-08-12.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
